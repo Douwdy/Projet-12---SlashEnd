c5175c2afadbb7537b5ab7a1cadd4eea1f0e01f2
Guillaume Lamarque <guillaumelamarque7@gmail.com>
2024-10-04 22:08:20 +0200

    Initialisation du projet, création d'un ébauche du Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+  return (
+    <header className="header">
+        <h1 className="header-title">SlashEnd</h1>
+        <div className="header-links">
+            <a href="#about">Présentation</a>
+            <a href="#techs">Technologies</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#contact">Contact</a>
+        </div>
+    </header>
+  );
+}
+
+export default Header;
