0793d2c0f9b6cb5b39fe774b3cf5078183545119
Guillaume Lamarque <guillaumelamarque7@gmail.com>
2024-10-16 09:49:55 +0200

    Réarrangement des boutons, des Typos, de la section hero, des paragraphes et début de la création du la section competences

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Strip from './components/Strip';
 import Presentation from './components/Presentation';
+import Competences from './components/Competences';
 import Footer from './components/Footer';
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Hero />
       <Strip angle={1} />
       <Presentation />
+      <Strip angle={-1} />
+      <Competences />
       <Footer />
     </>
   );
---
+++ src/components/Competences.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Paragraph from './Paragraph';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3Alt, faJs, faReact, faNodeJs, faFigma, faSass } from '@fortawesome/free-brands-svg-icons';
+const Competences = () => {
+
+    return (
+        <section className="competences">
+            <div className='competences-desc'>
+                <Paragraph title="Mes compétences">
+                En tant que Développeur web, je maîtrise l'HTML, le CSS avec SASS, ainsi que le JavaScript et le framework React.
+                Grâce à ces compétences, je crée des interfaces modernes, dynamiques et performantes. Mon expertise en structuration de code et
+                en optimisation des performances me permet de livrer des applications web réactives et modulaires,
+                toujours en accord avec les meilleures pratiques du développement.
+                </Paragraph>
+            </div>
+            <div className='competences-techs'>
+                <div className='competences-techs_dev'>
+                    <h2>Stack de développement</h2>
+                    <div className='competences-techs_dev-cards'>
+                        <div className='competences-techs_dev-cards-html'>
+                            <FontAwesomeIcon icon={faHtml5} style={{color: "#e34c26"}} size='3x'/>
+                            <p>HTML5</p>
+                        </div>
+                        <div className='competences-techs_dev-cards-css'>
+                            <FontAwesomeIcon icon={faCss3Alt} style={{color: "#1572b6"}} size='3x'/>
+                            <p>CSS3</p>
+                        </div>
+                        <div className='competences-techs_dev-cards-sass'>
+                            <FontAwesomeIcon icon={faSass} style={{color: "#cc6699"}} size='3x'/>
+                            <p>Sass</p>
+                        </div>
+                        <div className='competences-techs_dev-cards-js'>
+                            <FontAwesomeIcon icon={faJs} style={{color: "#f7df1e"}} size='3x'/>
+                            <p>JavaScript</p>
+                        </div>
+                        <div className='competences-techs_dev-cards-react'>
+                            <FontAwesomeIcon icon={faReact} style={{color: "#61dafb"}} size='3x'/>
+                            <p>React</p>
+                        </div>
+                        <div className='competences-techs_dev-cards-node'>
+                            <FontAwesomeIcon icon={faNodeJs} style={{color: "#339933"}} size='3x'/>
+                            <p>Node.js</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='competences-techs_design'>
+                    <h2>Stack de design</h2>
+                    <div className='competences-techs_design-cards'>
+                        <div className='competences-techs_design-cards-figma'>
+                            <FontAwesomeIcon icon={faFigma} style={{color: "#D3130D"}} size='3x'/>
+                            <p>Figma</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Competences;
