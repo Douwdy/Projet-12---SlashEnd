bb1fed0d36a7615f11625ce0d90c49208961a1df
Guillaume Lamarque <guillaumelamarque7@gmail.com>
2025-01-21 09:40:14 +0100

    Add hamburger menu

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import logo from '../../assets/img/Logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 
 const Header = () => {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header>
             <div>
                 <img src={logo} alt="Logo de SlashEnd"/>
             </div>
-            <div className="header_2">
-                <a href="#index">Accueil</a>
-                <a href="#presentation">Présentation</a>
-                <a href="#products">Produits</a>
-                <a href="#projects">Projets</a>
+            <div className="header">
+                <FontAwesomeIcon icon={faBars} onClick={toggleMenu} />
+                <nav className={isOpen ? 'open' : ''}>
+                    <a href="#index" onClick={toggleMenu}>Accueil</a>
+                    <a href="#presentation" onClick={toggleMenu}>Présentation</a>
+                    <a href="#skills" onClick={toggleMenu}>Mon parcours</a>
+                    <a href="#products" onClick={toggleMenu}>Produits</a>
+                    <a href="#projects" onClick={toggleMenu}>Projets</a>
+                </nav>
             </div>
         </header>
     );
